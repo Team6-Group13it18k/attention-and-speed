a1016bacfc205d6f48c764b3ed5c12c22ac0915a
Вадим Дунькин <vadimushka.d@gmail.com>
2018-02-03 15:51:35 +0300

    Init repositories

---
+++ desktop/src/team6/g13it18k/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package team6.g13it18k.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import team6.g13it18k.ASGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ASGame(), config);
+	}
+}
