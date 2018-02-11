4522cbcedccd935ec1ed38da8ca8eb503da3ef1f
Вадим Дунькин <vadimushka.d@gmail.com>
2018-02-11 22:22:10 +0300

    Реализован splash экран с показом лого

---
+++ core/src/team6/g13it18k/screens/SplashScreen.java
@@ -0,0 +1,90 @@
+package team6.g13it18k.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.*;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+import team6.g13it18k.ASGame;
+
+/**
+ * Данный класс реализует окно заставки
+ */
+public class SplashScreen implements Screen {
+
+    private Texture splashImage;
+    private Stage stage;
+
+    private final ASGame game;
+
+    public SplashScreen(final ASGame gam) {
+        game = gam;
+
+        splashImage = new Texture("logo.png");
+        splashImage.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+        TextureRegion textureRegion = new TextureRegion(splashImage, 0, 0, 512, 128);
+
+        stage = new Stage();
+
+        Image actorImage = new Image(textureRegion);
+
+        float width = Gdx.graphics.getWidth();
+        float height = Gdx.graphics.getHeight();
+        float desiredWidth = width * .9f;
+        float scale = desiredWidth / actorImage.getWidth();
+
+        actorImage.setSize(actorImage.getWidth() * scale, actorImage.getHeight() * scale);
+        actorImage.setPosition(width / 2 - actorImage.getWidth() / 2, height / 2 - actorImage.getHeight() / 2);
+        actorImage.addAction(
+                sequence(
+                        fadeOut(4),
+                        run(
+                                new Runnable(){
+                                    @Override
+                                    public void run() {
+                                        game.setScreen(new MenuScreen(game));
+                                        dispose();
+                                    }
+                                }
+                        )
+                )
+        );
+        stage.addActor(actorImage);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act(delta);
+        stage.draw();
+    }
+
+    @Override
+    public void show() {}
+
+    @Override
+    public void resize(int width, int height) {}
+
+    @Override
+    public void pause() {}
+
+    @Override
+    public void resume() {}
+
+    @Override
+    public void hide() {}
+
+    @Override
+    public void dispose() {
+        splashImage.dispose();
+        stage.dispose();
+        game.dispose();
+    }
+}
