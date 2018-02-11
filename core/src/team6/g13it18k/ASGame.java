e1849fa20066a54d5b7e14c62efd2675c4c2c097
Вадим Дунькин <vadimushka.d@gmail.com>
2018-02-11 22:22:59 +0300

    Реализован главный класс игры

--- core/src/team6/g13it18k/ASGame.java
+++ core/src/team6/g13it18k/ASGame.java
@@ -1,33 +1,41 @@
 package team6.g13it18k;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 
-public class ASGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+import team6.g13it18k.objects.BackgroundActor;
+import team6.g13it18k.screens.SplashScreen;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+/**
+ * Главный класс игры, все движуха идет через этот класс
+ */
+public class ASGame extends Game {
+
+    public BackgroundActor background;
+
+    public BitmapFont font, levels;
+    private static final String FONT_CHARACTERS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789][_!$%#@|\\/?-+=()*&.;,{}\"´`'<>";
+
+    @Override
+    public void create() {
+        background = new BackgroundActor();
+        background.setPosition(0, 0);
+
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/YanoneKaffeesatz-Regular.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter param = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        param.size = Gdx.graphics.getHeight() / 18; // Размер шрифта. Я сделал его исходя из размеров экрана. Правда коряво, но вы сами можете поиграться, как вам угодно.
+        param.characters = FONT_CHARACTERS; // Наши символы
+        font = generator.generateFont(param); // Генерируем шрифт
+        param.size = Gdx.graphics.getHeight() / 20;
+        levels = generator.generateFont(param);
+        font.setColor(Color.WHITE); // Цвет белый
+        levels.setColor(Color.WHITE);
+        generator.dispose();
+
+        setScreen(new SplashScreen(this));
+    }
+
 }
