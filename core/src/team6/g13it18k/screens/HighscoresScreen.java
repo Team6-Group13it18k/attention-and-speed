c91944307a1b4ff1c3ec98fc6d7b57ca42f911ea
Вадим Дунькин <vadimushka.d@gmail.com>
2018-02-11 22:21:42 +0300

    Подготовлены шаблоны игровый экранов

---
+++ core/src/team6/g13it18k/screens/HighscoresScreen.java
@@ -0,0 +1,54 @@
+package team6.g13it18k.screens;
+
+
+import com.badlogic.gdx.Screen;
+
+import team6.g13it18k.ASGame;
+
+/**
+ * Данный класс реализует окно рекордов
+ */
+public class HighscoresScreen implements Screen {
+
+    private final ASGame game;
+
+    public HighscoresScreen(final ASGame gam) {
+        game = gam;
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
---
+++ core/src/team6/g13it18k/screens/LevelScreen.java
@@ -0,0 +1,65 @@
+package team6.g13it18k.screens;
+
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+import team6.g13it18k.ASGame;
+
+/**
+ * Данный класс реализует окно выбора уровней
+ */
+public class LevelScreen implements Screen {
+
+    private final ASGame game;
+
+    private Stage stage;
+
+    LevelScreen(final ASGame gam) {
+        this.game = gam;
+
+        stage = new Stage();
+        stage.addActor(game.background);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act(delta);
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
