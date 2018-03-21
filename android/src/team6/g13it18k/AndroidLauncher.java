3ef359374d7e8e098e47d43d8bf645d974db28dd
Вадим Дунькин <vadimushka.d@gmail.com>
2018-03-21 11:07:48 +0300

    javadoc

--- android/src/team6/g13it18k/AndroidLauncher.java
+++ android/src/team6/g13it18k/AndroidLauncher.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
+/**
+ * Class AndroidLauncher
+ *
+ * Запуска игры для андроид
+ */
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
--- desktop/src/team6/g13it18k/desktop/DesktopLauncher.java
+++ desktop/src/team6/g13it18k/desktop/DesktopLauncher.java
@@ -4,6 +4,11 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import team6.g13it18k.ASGame;
 
+/**
+ * Class DesktopLauncher
+ *
+ * Запуска игры для настольного компьютера
+ */
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
